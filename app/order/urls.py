726c7c2ec328e7b4a21bb398bcc696e0bb510eba
Valerii <v.bill2003@gmail.com>
2024-11-01 11:05:43 +0200

    Ready GetOrdersForOfficeView to get all orders for special office by id

--- app/order/urls.py
+++ app/order/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import OrderCreateView
+from .views import GetOrdersForOfficeView, OrderCreateView
 
 app_name = 'order'
 
 urlpatterns = [
     path('order/', OrderCreateView.as_view(), name='order'),
+    path('get-all-orders-for-office/<int:pk>/', GetOrdersForOfficeView.as_view(), name='get-all-orders-for-office')
 ]
--- app/order/views.py
+++ app/order/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core.models import Company, Office, Provider, User, Utilities
+from core.models import Company, Office, Order, Provider, User, Utilities
 
 from .serializers import OrderSerializer
 
@@ -16,12 +16,10 @@ class OrderCreateView(APIView):
         user = request.user
         office_id = data.get('office')
 
-        print(f"User: {user}, Office ID: {office_id}")
-
         if user.user_type == User.OWNER_USER:
             if not Office.objects.filter(pk=office_id, company__owner=user).exists():
                 return Response(
-                    {"error": "You do not own this company or it does not exist."},
+                    {"error": "You do not own this office or it does not exist."},
                     status=status.HTTP_404_NOT_FOUND
                 )
 
@@ -30,13 +28,13 @@ class OrderCreateView(APIView):
                 Office.objects.get(pk=office_id, manager=user)
             except Office.DoesNotExist:
                 return Response(
-                    {"error": "No associated company found for this manager."},
+                    {"error": "No associated office found for this manager."},
                     status=status.HTTP_404_NOT_FOUND
                 )
 
         else:
             return Response(
-                {"detail": "You do not have permission to create a provider."},
+                {"detail": "You do not have permission to create an order."},
                 status=status.HTTP_403_FORBIDDEN
             )
 
@@ -50,3 +48,37 @@ class OrderCreateView(APIView):
                 status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GetOrdersForOfficeView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, pk):
+        user = request.user
+        office_id = pk
+
+        if user.user_type == User.OWNER_USER:
+            if not Office.objects.filter(pk=office_id, company__owner=user).exists():
+                return Response(
+                    {"error": "You do not own this office or it does not exist."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        elif user.user_type == User.MANAGER_USER:
+            try:
+                Office.objects.get(pk=office_id, manager=user)
+            except Office.DoesNotExist:
+                return Response(
+                    {"error": "No associated office found for this manager."},
+                    status=status.HTTP_404_NOT_FOUND
+                )
+
+        else:
+            return Response(
+                {"detail": "You do not have permission to create an order."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        order = Order.objects.filter(office=office_id)
+        serializer = OrderSerializer(order, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
