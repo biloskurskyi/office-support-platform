d3b51c79a39b3042686f4d4ced3f10555e05ba52
Valerii <v.bill2003@gmail.com>
2025-04-03 18:59:51 +0300

    Fixed CreateCompanyForm.tsx

--- frontend/src/components/CreateCompanyForm/CreateCompanyForm.tsx
+++ frontend/src/components/CreateCompanyForm/CreateCompanyForm.tsx
@@ -105,7 +105,7 @@ const CreateCompanyForm = () => {
               name="website"
               value={formData.website}
               onChange={handleInputChange}
-              type="password"
+              // type="password"
             />
 
             <ul>
