c1de3f6228b2695080cc53d75f21c5d22e980991
Valerii <v.bill2003@gmail.com>
2025-05-05 18:57:30 +0300

    Fixed App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import './styles/App.css';
-import { Outlet } from 'react-router-dom';
+import {Outlet} from 'react-router-dom';
 import Footer from './components/Footer/Footer.tsx';
 import BackgroundImage from './components/BackgroundImage/BackgroundImage.tsx';
 import Header from './components/Header/Header.tsx';
-import { DataProvider } from './context/useDataCompanyOffice.tsx';
+import {DataProvider} from './context/useDataCompanyOffice.tsx';
 
 const App = () => {
-  const [text, setText] = useState<React.ReactNode>(<></>);
+    const [text, setText] = useState<React.ReactNode>(<></>);
 
-  return (
-    <DataProvider>
-      <div>
-        <BackgroundImage text={text} />
-        <Header />
-        <Outlet context={{ setText }} />
-        <Footer />
-      </div>
-    </DataProvider>
-  );
+    return (
+        <DataProvider>
+            <div>
+                <BackgroundImage text={text}/>
+                <Header/>
+                <Outlet context={{setText}}/>
+                <Footer/>
+            </div>
+        </DataProvider>
+    );
 };
 
 export default App;
